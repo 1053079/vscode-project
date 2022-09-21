b8e7b3c4c63168d2e60980f8089becbb93ec060e
Yong Pok Low <yongpok95@live.nl>
2022-09-21 22:17:30 +0200

    blasx

--- pygame/pygame1.py
+++ pygame/pygame1.py
@@ -12,4 +12,3 @@ pygame.display.set_caption("Hello World Printer")
 myFont = pygame.font.Sysfont("Segoe UI",90)
 HelloWorld = myFont.render("Hello World",1, (255, 0, 255), (255, 255, 255))
 
-except
